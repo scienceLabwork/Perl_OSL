233c8dda37a2f685da9c49f6f84504cd57abd8d1
Rudra Shah <heysiri876@gmail.com>
2022-08-17 01:53:24 +0530

    v8

---
+++ lab4extended.py
@@ -0,0 +1,20 @@
+import cv2
+
+def nothing(arg): pass
+
+img = cv2.VideoCapture(0)
+cv2.namedWindow('Filter')
+cv2.createTrackbar('filter', 'Filter', 0, 1, nothing)
+
+while True:
+    _, frame = img.read()
+    frame = cv2.flip(frame, 1)
+    count = cv2.getTrackbarPos('Zoom', 'Filter')
+    print("from loop",count)
+    if(count==0):
+        cv2.imshow('Filter', frame)
+    else:
+        filter4 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Filter', filter4)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
