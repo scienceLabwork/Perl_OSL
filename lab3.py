066c00512a6b8a2a5a728c77f6f87ba398a8306b
Rudra Shah <heysiri876@gmail.com>
2022-08-17 01:05:17 +0530

    v6

--- lab3.py
+++ lab3.py
@@ -1,13 +1,28 @@
 import cv2
 
-x = cv2.imread('lab3sample.png')
-black = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Original Image', x)
-cv2.imshow('Black and white', black)
-cv2.waitKey(0)
-if cv2.waitKey(115) & 0x73 == ord('s'):
-    cv2.imwrite('lab3bnw.png', black)
-    cv2.destroyAllWindows()
-    print('Image saved')
-if cv2.waitKey(1) & 0xFF == ord('q'):
-    cv2.destroyAllWindows()
+def nothing(arg): pass
+
+cv2.namedWindow('Image')
+cv2.createTrackbar('Zoom', 'Image', 0, 100, nothing)
+while(1):
+    cv2.namedWindow('Image', cv2.WINDOW_KEEPRATIO)
+    img = cv2.imread('lab3sample.png')
+    black = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    cv2.imshow('Image', img)
+    threshold = cv2.getTrackbarPos('Zoom', 'Image')
+    print("from loop",threshold)
+
+    h,w = img.shape[0], img.shape[1]
+    x,y = threshold*3, threshold*2
+    img = img[x:h+x, y:w-y]
+    print(h,w,x,y)
+    cv2.imshow('Image', img)
+    cv2.imshow('Black and white', black)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite('lab3bnw.png', black)
+        cv2.destroyAllWindows()
+        break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
+cv2.destroyAllWindows()
